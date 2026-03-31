197ca6fbc2e95e8905f066eed1569e6a4c249d85
iGAP Tech <kishor@igaptechnologies.com>
2026-03-31 14:34:39 +0530

    app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ app.register_blueprint(auth)
 app.register_blueprint(admin)
 app.register_blueprint(user)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
