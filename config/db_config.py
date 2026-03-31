cc63ec8f5d986ba4f784ba61eaa0c760c1e73f9a
iGAP Tech <kishor@igaptechnologies.com>
2026-03-31 14:41:25 +0530

    sql

--- config/db_config.py
+++ config/db_config.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="admin123",
-        database="quiz_db"
+        host=os.getenv('MYSQL_HOST'),
+        port=int(os.getenv('MYSQL_PORT')),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        database=os.getenv('MYSQL_DATABASE')
     )
     return connection
